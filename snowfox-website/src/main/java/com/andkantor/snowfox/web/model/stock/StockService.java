e36bfde394c9d12057fb67e8f38e814cc66727f6
Andras Kantor <and.kantor@gmail.com>
2017-11-17 09:45:59 +0100

    Polishing

--- snowfox-website/src/main/java/com/andkantor/snowfox/web/model/stock/StockService.java
+++ snowfox-website/src/main/java/com/andkantor/snowfox/web/model/stock/StockService.java
@@ -2,10 +2,8 @@ package com.andkantor.snowfox.web.model.stock;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import com.andkantor.snowfox.web.model.product.Product;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -15,9 +13,6 @@ public class StockService {
 
     @Value("${snowfox.stock-service-url}")
     private String stockServiceUrl;
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     public List<StockInfo> getStockInfo(List<Product> products) {
         throw new NotImplementedException();
